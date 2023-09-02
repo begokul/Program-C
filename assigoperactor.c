a2342199c6255b748585a6de212006664882843e
Gokul <141204889+lovelygoku@users.noreply.github.com>
2023-09-02 10:31:25 +0530

    Add files via upload

---
+++ assigoperactor.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int price;
+    printf("Enter the price of product :");
+    scanf("%d",&price);
+    (price>=20000)?printf("You have 10 percentage of discount"):printf("you have 5 percentage of discount");
+	return 0;
+}
+
+#price, discount, final price
